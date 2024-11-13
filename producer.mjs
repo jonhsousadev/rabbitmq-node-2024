c3255e53ed6f292e3e2188d2c0533d67cf303413
Jonh Sousa <jonhnbsousa@gmail.com>
2024-11-13 00:25:00 -0300

    feat - Criando primeira mensagens e as enviando paara a fila

--- producer.mjs
+++ producer.mjs
@@ -10,6 +10,17 @@ async function main() {
 
   const channel = await connection.createChannel()
 
+  await channel.assertQueue('minha_fila', {
+    durable: true,
+  })
+
+  await channel.publish('','minha_fila', Buffer.from("Minha mensagem!"))
+  await channel.publish('', 'minha_fila', Buffer.from("Minha mensagem 2"))
+
+  await channel.sendToQueue('minha_fila', Buffer.from("Mais uma mensagem"))
+
+  channel.close()
+  connection.close()
 }
 
 main()
